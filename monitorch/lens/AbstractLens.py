0013f76280944c0375ea7762b6eda264aab67034
Maksym Khavil <mk.khavil@gmail.com>
2025-06-18 17:31:53 +0200

    ft: add abstract lenses

---
+++ monitorch/lens/AbstractLens.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import Any, Type
+from monitorch.preprocessor import AbstractForwardPreprocessor, AbstractBackwardPreprocessor, AbstractPreprocessor
+from monitorch.vizualizer import AbstractVizualizer
+
+
+class AbstractLens(ABC):
+
+    @abstractmethod
+    def requires_forward(self) -> set[Type[AbstractForwardPreprocessor]]:
+        pass
+
+    @abstractmethod
+    def requires_backward(self) -> set[Type[AbstractBackwardPreprocessor]]:
+        pass
+
+    @abstractmethod
+    def register_preprocessors(self, preprocessors : list[AbstractPreprocessor]):
+        pass
+
+    @abstractmethod
+    def vizualize(self, vizualizer : AbstractVizualizer):
+        pass
---
+++ monitorch/lens/__init__.py
@@ -0,0 +1,1 @@
+from AbstractLens import AbstractLens
