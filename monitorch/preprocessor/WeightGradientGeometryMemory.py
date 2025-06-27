fix: restructure gradient preprocessors

monitorch/preprocessor/WeightGradientGeometryMemory.py
@@ -5,10 +5,10 @@ from torch.linalg import vector_norm
 from torch import no_grad
 
 from ._module_classes import islinear
-from .AbstractBackwardPreprocessor import AbstractBackwardPreprocessor
+from .AbstractGradientPreprocessor import AbstractGradientPreprocessor
 
 
-class WeightGradientGeometryMemory(AbstractBackwardPreprocessor):
+class WeightGradientGeometryMemory(AbstractGradientPreprocessor):
 
     def __init__(self, adj_prod, normalize):
         self._adj_prod = adj_prod
@@ -17,9 +17,8 @@ class WeightGradientGeometryMemory(AbstractBackwardPreprocessor):
         if adj_prod:
             self._prev_grad = {}
 
-    def process(self, name : str, module, grad_input, grad_output) -> None:
+    def process_grad(self, name : str, grad) -> None:
         l = self._value.setdefault(name, [])
-        grad = module.weight.grad
         new_norm = vector_norm(grad)
         if self._normalize:
             new_norm /= sqrt(grad.numel())

monitorch/preprocessor/WeightGradientGeometryRunning.py
@@ -5,11 +5,11 @@ from torch import no_grad
 from torch.nn import Module
 from torch.linalg import vector_norm
 
-from .AbstractBackwardPreprocessor import AbstractBackwardPreprocessor
+from .AbstractGradientPreprocessor import AbstractGradientPreprocessor
 from monitorch.numerical import RunningMeanVar
 from ._module_classes import islinear
 
-class WeightGradientGeometryRunning(AbstractBackwardPreprocessor):
+class WeightGradientGeometryRunning(AbstractGradientPreprocessor):
 
     def __init__(self, adj_prod : bool, normalize : bool):
         self._adj_prod = adj_prod
@@ -19,8 +19,7 @@ class WeightGradientGeometryRunning(AbstractBackwardPreprocessor):
             self._prev_grad = {}
             self._prev_norm = {}
 
-    def process(self, name : str, module, grad_input, grad_output) -> None:
-        grad = module.weight.grad
+    def process_grad(self, name : str, grad) -> None:
         new_norm = vector_norm(grad)
         if self._adj_prod:
 