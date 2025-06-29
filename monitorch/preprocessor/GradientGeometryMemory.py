test&fix: gradient, backward gatherer, output grad

monitorch/preprocessor/GradientGeometryMemory.py
@@ -2,13 +2,13 @@
 from math import sqrt
 from typing import Any
 from torch.linalg import vector_norm
-from torch import no_grad
+from copy import deepcopy
 
 from ._module_classes import islinear
 from .AbstractGradientPreprocessor import AbstractGradientPreprocessor
 
 
-class WeightGradientGeometryMemory(AbstractGradientPreprocessor):
+class GradientGeometryMemory(AbstractGradientPreprocessor):
 
     def __init__(self, adj_prod, normalize):
         self._adj_prod = adj_prod
@@ -19,19 +19,16 @@ class WeightGradientGeometryMemory(AbstractGradientPreprocessor):
 
     def process_grad(self, name : str, grad) -> None:
         l = self._value.setdefault(name, [])
-        new_norm = vector_norm(grad)
+        new_norm = vector_norm(grad).item()
         if self._normalize:
             new_norm /= sqrt(grad.numel())
 
         if self._adj_prod:
-
-            # Computes dot product of normalised current and previous gradients
             prev_norm = l[-1][0] if l else 1.0
+            new_prod = (grad * self._prev_grad.get(name, 0.0)).sum().item() / (new_norm * prev_norm)
             if self._normalize:
-                prev_norm *= sqrt(grad.numel())
-            new_prod = (grad * self._prev_grad.get(name, 0.0)).sum() / (new_norm * prev_norm)
-
-            self._prev_grad[name] = grad
+                new_prod /= grad.numel()
+            self._prev_grad[name] = deepcopy(grad)
             l.append( (new_norm, new_prod) )
         else:
             l.append(new_norm)

monitorch/preprocessor/GradientGeometryRunning.py
@@ -1,5 +1,6 @@
 
 from math import sqrt
+from copy import deepcopy
 from typing import Any
 from torch import no_grad
 from torch.nn import Module
@@ -9,7 +10,7 @@ from .AbstractGradientPreprocessor import AbstractGradientPreprocessor
 from monitorch.numerical import RunningMeanVar
 from ._module_classes import islinear
 
-class BiasGradientGeometryRunning(AbstractGradientPreprocessor):
+class GradientGeometryRunning(AbstractGradientPreprocessor):
 
     def __init__(self, adj_prod : bool, normalize : bool):
         self._adj_prod = adj_prod
@@ -20,22 +21,24 @@ class BiasGradientGeometryRunning(AbstractGradientPreprocessor):
             self._prev_norm = {}
 
     def process_grad(self, name : str, grad) -> None:
-        new_norm = vector_norm(grad)
+        new_norm = vector_norm(grad).item()
+        if self._normalize:
+            new_norm /= sqrt(grad.numel())
         if self._adj_prod:
+            new_prod = (grad * self._prev_grad.get(name, 0.0)).sum().item() / (new_norm * self._prev_norm.get(name, 1.0))
+            if self._normalize:
+                new_prod /= grad.numel()
 
-            # Computes dot product of normalised current and previous gradients
-            new_prod = (grad * self._prev_grad.get(name, 0.0)).sum() / (new_norm * self._prev_norm.get(name, 1.0))
-
-            self._prev_grad[name] = grad
+            self._prev_grad[name] = deepcopy(grad)
             self._prev_norm[name] = new_norm
 
             norm, prod = self._value.setdefault(name, (RunningMeanVar(), RunningMeanVar()))
-            norm.update( (new_norm / sqrt(grad.numel())) if self._normalize else new_norm)
+            norm.update(new_norm)
             prod.update(new_prod)
 
         else:
             norm = self._value.setdefault(name, RunningMeanVar())
-            norm.update( (new_norm / sqrt(grad.numel())) if self._normalize else new_norm)
+            norm.update(new_norm)
 
     @property
     def value(self) -> dict[str, Any]:

monitorch/preprocessor/OutputGradientGeometryMemory.py
@@ -1,5 +1,6 @@
 
 from math import sqrt
+from copy import deepcopy
 from typing import Any
 from torch.linalg import vector_norm
 from torch import no_grad
@@ -19,21 +20,18 @@ class OutputGradientGeometryMemory(AbstractBackwardPreprocessor):
 
     @no_grad
     def process(self, name : str, module, grad_input, grad_output) -> None:
+        grad = grad_output[0]
         l = self._value.setdefault(name, [])
-        grad = grad_output
-        new_norm = vector_norm(grad)
+        new_norm = vector_norm(grad).item()
         if self._normalize:
             new_norm /= sqrt(grad.numel())
 
         if self._adj_prod:
-
-            # Computes dot product of normalised current and previous gradients
-            prev_norm = l[-1] if l else 1.0
+            prev_norm = l[-1][0] if l else 1.0
+            new_prod = (grad * self._prev_grad.get(name, 0.0)).sum().item() / (new_norm * prev_norm)
             if self._normalize:
-                prev_norm *= sqrt(grad.numel())
-            new_prod = (grad * self._prev_grad.get(name, 0.0)).sum() / (new_norm * prev_norm)
-
-            self._prev_grad[name] = grad
+                new_prod /= grad.numel()
+            self._prev_grad[name] = deepcopy(grad)
             l.append( (new_norm, new_prod) )
         else:
             l.append(new_norm)

monitorch/preprocessor/OutputGradientGeometryRunning.py
@@ -1,5 +1,6 @@
 
 from math import sqrt
+from copy import deepcopy
 from typing import Any
 from torch import no_grad
 from torch.nn import Module
@@ -21,23 +22,26 @@ class OutputGradientGeometryRunning(AbstractBackwardPreprocessor):
 
     @no_grad
     def process(self, name : str, module, grad_input, grad_output) -> None:
-        grad = grad_output
-        new_norm = vector_norm(grad)
+        grad = grad_output[0]
+        new_norm = vector_norm(grad).item()
+        if self._normalize:
+            new_norm /= sqrt(grad.numel())
+
         if self._adj_prod:
+            new_prod = (grad * self._prev_grad.get(name, 0.0)).sum().item() / (new_norm * self._prev_norm.get(name, 1.0))
+            if self._normalize:
+                new_prod /= grad.numel()
 
-            # Computes dot product of normalised current and previous gradients
-            new_prod = (grad * self._prev_grad.get(name, 0.0)).sum() / (new_norm * self._prev_norm.get(name, 1.0))
-
-            self._prev_grad[name] = grad
+            self._prev_grad[name] = deepcopy(grad)
             self._prev_norm[name] = new_norm
 
             norm, prod = self._value.setdefault(name, (RunningMeanVar(), RunningMeanVar()))
-            norm.update( (new_norm / sqrt(grad.numel())) if self._normalize else new_norm)
+            norm.update(new_norm)
             prod.update(new_prod)
 
         else:
             norm = self._value.setdefault(name, RunningMeanVar())
-            norm.update( (new_norm / sqrt(grad.numel())) if self._normalize else new_norm)
+            norm.update(new_norm)
 
     @property
     def value(self) -> dict[str, Any]:

tests/test_preprocessor/test_gradient_geometry.py
@@ -1,5 +1,5 @@
-from math import sqrt
 from copy import deepcopy
+from math import sqrt
 import pytest
 
 import torch
@@ -8,12 +8,11 @@ import torch.nn as nn
 from torch.linalg import vector_norm
 
 from monitorch.preprocessor import (
-        WeightGradientGeometryMemory, WeightGradientGeometryRunning,
-        BiasGradientGeometryMemory,   BiasGradientGeometryRunning,
+        GradientGeometryMemory, GradientGeometryRunning,
         OutputGradientGeometryMemory, OutputGradientGeometryRunning,
 )
 
-from monitorch.gatherer import WeightGradientGatherer, BiasGradientGatherer
+from monitorch.gatherer import WeightGradientGatherer, BiasGradientGatherer, BackwardGatherer
 
 def replace_w_grad(tensor):
     def f(module, inp, out):
@@ -46,10 +45,10 @@ def replace_b_grad(tensor):
     ]
 )
 def test_artificial_gradient_norm(module, inp_size, grad_w, grad_b, normalize):
-    wggm = WeightGradientGeometryMemory(adj_prod=False, normalize=normalize)
-    wggr = WeightGradientGeometryRunning(adj_prod=False, normalize=normalize)
-    bggm = BiasGradientGeometryMemory(adj_prod=False, normalize=normalize)
-    bggr = BiasGradientGeometryRunning(adj_prod=False, normalize=normalize)
+    wggm = GradientGeometryMemory(adj_prod=False, normalize=normalize)
+    wggr = GradientGeometryRunning(adj_prod=False, normalize=normalize)
+    bggm = GradientGeometryMemory(adj_prod=False, normalize=normalize)
+    bggr = GradientGeometryRunning(adj_prod=False, normalize=normalize)
 
     module.register_full_backward_hook(replace_w_grad(grad_w))
     module.register_full_backward_hook(replace_b_grad(grad_b))
@@ -99,10 +98,10 @@ def test_artificial_gradient_norm(module, inp_size, grad_w, grad_b, normalize):
     ]
 )
 def test_sequence_gradient_norm(module, inp_size, normalize, n_iter, seed):
-    wggm = WeightGradientGeometryMemory(adj_prod=True, normalize=normalize)
-    wggr = WeightGradientGeometryRunning(adj_prod=True, normalize=normalize)
-    bggm = BiasGradientGeometryMemory(adj_prod=True, normalize=normalize)
-    bggr = BiasGradientGeometryRunning(adj_prod=True, normalize=normalize)
+    wggm = GradientGeometryMemory(adj_prod=True, normalize=normalize)
+    wggr = GradientGeometryRunning(adj_prod=True, normalize=normalize)
+    bggm = GradientGeometryMemory(adj_prod=True, normalize=normalize)
+    bggr = GradientGeometryRunning(adj_prod=True, normalize=normalize)
 
     wgg = WeightGradientGatherer(
         module, [wggm, wggr], 'standalone_test'
@@ -120,20 +119,27 @@ def test_sequence_gradient_norm(module, inp_size, normalize, n_iter, seed):
     b_products = []
 
     torch.manual_seed(seed)
-    for _ in range(n_iter):
+    for i in range(n_iter):
         x.cauchy_()
         module(x).square().mean().backward()
 
-        w_norm = vector_norm(module.weight.grad)
-        b_norm = vector_norm(module.bias.grad)
-        w_prod = (prev_w_grad * module.weight.grad).sum() / (w_norm * w_norms[-1])
-        b_prod = (prev_b_grad * module.bias.grad).sum()   / (b_norm * b_norms[-1])
+        w_norm = vector_norm(module.weight.grad).item()
+        b_norm = vector_norm(module.bias.grad).item()
         if normalize:
             w_norm /= sqrt(module.weight.grad.numel())
             b_norm /= sqrt(module.bias.grad.numel())
+        w_prod = (prev_w_grad * module.weight.grad).sum().item() / (w_norm * w_norms[-1])
+        b_prod = (prev_b_grad * module.bias.grad).sum().item()   / (b_norm * b_norms[-1])
+
+        if normalize:
+            w_prod /= module.weight.grad.numel()
+            b_prod /= module.bias.grad.numel()
 
         assert np.isclose(w_norm, wggm.value['standalone_test'][-1][0])
         assert np.isclose(b_norm, bggm.value['standalone_test'][-1][0])
+
+        assert abs(w_prod) <= (1 + 1e-5)
+        assert abs(b_prod) <= (1 + 1e-5)
 
         assert np.isclose(w_prod, wggm.value['standalone_test'][-1][1])
         assert np.isclose(b_prod, bggm.value['standalone_test'][-1][1])
@@ -142,8 +148,8 @@ def test_sequence_gradient_norm(module, inp_size, normalize, n_iter, seed):
         w_products.append(w_prod)
         b_norms.append(b_norm)
         b_products.append(b_prod)
-        prev_w_grad = module.weight.grad
-        prev_b_grad = module.bias.grad
+        prev_w_grad = deepcopy(module.weight.grad)
+        prev_b_grad = deepcopy(module.bias.grad)
 
     del w_norms[0]
     del b_norms[0]
@@ -154,9 +160,97 @@ def test_sequence_gradient_norm(module, inp_size, normalize, n_iter, seed):
         [rmv.mean, rmv.var, rmv.min_, rmv.max_]
     ).all()
 
+    rmv = wggr.value['standalone_test'][1]
+    assert np.isclose(
+        [np.mean(w_products), np.var(w_products), np.min(w_products), np.max(w_products)],
+        [rmv.mean, rmv.var, rmv.min_, rmv.max_]
+    ).all()
+
     rmv = bggr.value['standalone_test'][0]
     assert np.isclose(
         [np.mean(b_norms), np.var(b_norms), np.min(b_norms), np.max(b_norms)],
         [rmv.mean, rmv.var, rmv.min_, rmv.max_]
     ).all()
 
+    rmv = bggr.value['standalone_test'][1]
+    assert np.isclose(
+        [np.mean(b_products), np.var(b_products), np.min(b_products), np.max(b_products)],
+        [rmv.mean, rmv.var, rmv.min_, rmv.max_]
+    ).all()
+
+
+
+
+def collect_output_grad(l : list):
+    def f(module, grad_inp, grad_out):
+        l.append(deepcopy(grad_out[0]))
+    return f
+
+@pytest.mark.parametrize(
+    ['module', 'inp_size', 'normalize', 'n_iter', 'seed'],
+    [
+        (nn.Linear(10, 5),   (10,),       False, 100, 0),
+        (nn.Conv2d(1, 5, 2), (1, 16, 16), False, 100, 0),
+        (nn.Conv1d(1, 5, 2), (1, 16),     False, 100, 0),
+
+        (nn.Linear(10, 5),   (10,),       True,  100, 0),
+        (nn.Conv2d(1, 5, 2), (1, 16, 16), True,  100, 0),
+        (nn.Conv1d(1, 5, 2), (1, 16),     True,  100, 0),
+
+        (nn.Linear(10, 5),   (10,),       False, 100, 42),
+        (nn.Conv2d(1, 5, 2), (1, 16, 16), False, 100, 42),
+        (nn.Conv1d(1, 5, 2), (1, 16),     False, 100, 42),
+
+        (nn.Linear(10, 5),   (10,),       True,  100, 42),
+        (nn.Conv2d(1, 5, 2), (1, 16, 16), True,  100, 42),
+        (nn.Conv1d(1, 5, 2), (1, 16),     True,  100, 42),
+    ]
+)
+def test_sequence_output_gradient_norm(module, inp_size, normalize, n_iter, seed):
+    oggm = OutputGradientGeometryMemory(adj_prod=True, normalize=normalize)
+    oggr = OutputGradientGeometryRunning(adj_prod=True, normalize=normalize)
+
+    bg = BackwardGatherer(
+        module, [oggm, oggr], 'standalone_test'
+    )
+
+    x = torch.zeros(*inp_size)
+    norms = [1]
+    products = []
+    grads = [torch.tensor(0)]
+
+    module.register_full_backward_hook(collect_output_grad(grads))
+
+    torch.manual_seed(seed)
+    for _ in range(n_iter):
+        x.cauchy_()
+        module(x).square().mean().backward()
+
+        norm = vector_norm(grads[-1]).item()
+        if normalize:
+            norm /= sqrt(grads[-1].numel())
+        prod = (grads[-2] * grads[-1]).sum().item() / (norm * norms[-1])
+        if normalize:
+            prod /= grads[-1].numel()
+
+        assert np.isclose(norm, oggm.value['standalone_test'][-1][0])
+
+        assert abs(prod) <= (1 + 1e-5)
+        assert np.isclose(prod, oggm.value['standalone_test'][-1][1])
+
+        norms.append(norm)
+        products.append(prod)
+
+    del norms[0]
+    del grads
+
+    rmv = oggr.value['standalone_test'][0]
+    assert np.isclose(
+        [np.mean(norms), np.var(norms), np.min(norms), np.max(norms)],
+        [rmv.mean, rmv.var, rmv.min_, rmv.max_]
+    ).all()
+    rmv = oggr.value['standalone_test'][1]
+    assert np.isclose(
+        [np.mean(products), np.var(products), np.min(products), np.max(products)],
+        [rmv.mean, rmv.var, rmv.min_, rmv.max_]
+    ).all()