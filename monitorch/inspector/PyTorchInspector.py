b53b39e8c2075bf5b7698668c1bd569616c8eda1
Maksym Khavil <mk.khavil@gmail.com>
2025-08-10 15:39:17 +0200

    docs: inspector

--- monitorch/inspector/PyTorchInspector.py
+++ monitorch/inspector/PyTorchInspector.py
@@ -1,6 +1,3 @@
-"""
-    An inspector for neural networks that implement PyTorch nn.Module
-"""
 from torch.nn import Module
 from typing_extensions import Self
 
@@ -9,6 +6,97 @@ from monitorch.preprocessor import ExplicitCall
 from monitorch.visualizer import _vizualizer_dict, AbstractVisualizer
 
 class PyTorchInspector:
+    """
+    One class to rule them all.
+
+    ``PyTorchInspector`` is a class that manages interactions between lenses, visualizers and user defined module.
+
+    To use inspector one needs to initialize the inspector and provide it the module to monitor.
+    During initialization lenses from :mod:`monitorch.lens` must be provided.
+    The only thing that is required during training is to call :meth:`tick_epoch` on the end of each epoch.
+    Optionally one could push additional metrics using :meth:`push_metric` and :meth:`push_loss`.
+
+    If visualizer is ``'matplotlib'``, then ``'show_fig()'`` must be called on :attr:`visualizer`,
+    otherwise the plot will be drawn during training.
+
+    Parameters
+    ----------
+    lenses : list[AbstractLens]
+        List of objects from :mod:`monitorch.lens`, used to collect and plot data.
+
+    visualizer : str|AbstractVisualizer = 'matplotlib'
+        Visualizer to draw plots, must be either a visualizer object from :mod:`monitorch.visualizer`
+        or a string ``'matplotlib'``, ``'tensorboard'`` or ``'print'``.
+    module : None|torch.nn.Module = None
+        Optional neural network to examine, can be added later using :meth:`attach`.
+
+    depth : int = -1
+        Depth to unfold neural net injection tree. For example ``depth=0`` returns the model itself,
+        ``depth=1`` returns modules directly contained in ``module`` object. Default is ``depth=-1``,
+        that is to unfold until leaf modules are reached.
+    module_name_prefix : str = '.'
+        Delimiter to separate names of parent and child modules.
+
+    train_loss_str = 'train_loss'
+        String to be used for training loss.
+    non_train_loss_str = 'val_loss
+        String to be used for validation/testing/development loss.
+
+    Attributes
+    ----------
+    lenses : list[AbstractLens]
+        List of objects from :mod:`monitorch.lens`, used to collect and plot data.
+        Exatcly the same object as the one provided during initialization.
+
+    visualizer : AbstractVisualizer
+        Visualizaer object that draws all plots. Can be hot-swapped.
+
+    epoch_counter : int
+        Internal epoch counter used in :meth:`tick_epoch`, increments on call.
+
+    depth : int
+        Depth to unfold module inclusion tree.
+    module_name_prefix : str
+        Delimiter to separate names of parent and child modules.
+
+    Examples
+    --------
+
+    Basic usage with ``'LossMetrics'``, ``'OutputActivation'`` and ``'ParameterGradientGeometry'``
+    may look something like this.
+
+    >>> from monitorch.inspector import PyTorchInspector
+    >>> from monitorch.lens import LossMetrics, OutputActivation, ParameterGradientGeometry
+    >>> 
+    >>> loss_fn = nn.NLLLoss()
+    >>> 
+    >>> inspector = PyTorchInspector(
+    ...     lenses = [
+    ...         LossMetrics(loss_fn = loss_fn),
+    ...         OutputActivation(),
+    ...         ParameterGradientGeometry()
+    ...     ],
+    ...     module = mynet,
+    ...     visualizer='matplotlib'
+    ... )
+    >>> 
+    >>> for epoch in range(N_EPOCHS):
+    ...     for data, label in train_dataloader:
+    ...         optimizer.zero_grad()
+    ...         prediction = mynet(data)
+    ...         loss = loss_fn(prediction, label)
+    ...         loss.backward()
+    ...         optimizer.step()
+    ... 
+    ...     with torch.no_grad(): # outputs inside this block are not recorded
+    ...         for data, label in val_dataloader:
+    ...             prediction = mynet(data)
+    ...             loss = loss_fn(prediction, label)
+    ... 
+    ...     inspector.tick_epoch() # ticking the epoch
+    >>> 
+    >>> inspector.visualizer.show_fig()
+    """
 
     def __init__(
             self,
@@ -20,9 +108,10 @@ class PyTorchInspector:
             train_loss_str = 'train_loss',
             non_train_loss_str = 'val_loss'
     ):
-        """ Initializes all lenses and hooks to a module if one is given """
-        self._lenses = lenses
+        self.lenses = lenses
         self._call_preprocessor = ExplicitCall(train_loss_str, non_train_loss_str)
+        self.depth = depth
+        self.module_name_prefix = module_name_prefix
 
         self.epoch_counter = 0
 
@@ -33,37 +122,93 @@ class PyTorchInspector:
         else:
             self.visualizer : AbstractVisualizer = visualizer
 
-        for lens in self._lenses:
+        for lens in self.lenses:
             lens.register_foreign_preprocessor(self._call_preprocessor)
             lens.introduce_tags(self.visualizer)
         if module is not None:
-            self.attach(module, depth, module_name_prefix)
+            self.attach(module)
 
-    def attach(self, module : Module, depth : int, module_name_prefix='.') -> Self:
-        module_names = PyTorchInspector._module_leaves(module, depth, module_name_prefix)
+    def attach(self, module : Module) -> Self:
+        """
+        Attaches inspector to a module.
+
+        Unfolds inclusion module tree guided by ``depth`` set during initialization.
+        Registers submodules onto every lens.
+
+        Parameters
+        ----------
+        module : torch.nn.Module
+            Neural net to attach to.
+
+        Returns
+        -------
+        Self
+            Builder pattern.
+        """
+        module_names = PyTorchInspector._module_leaves(module, self.depth, self.module_name_prefix)
         for module, name in module_names:
-            for lens in self._lenses:
+            for lens in self.lenses:
                 lens.register_module(module, name)
         return self
 
     def detach(self) -> Self:
-        for lens in self._lenses:
+        """
+        Detaches all lenses from modules.
+
+        Returns
+        -------
+        Self
+            Builder pattern.
+        """
+        for lens in self.lenses:
             lens.detach_from_module()
         return self
 
     def push_metric(self, name : str, value : float, *, running : bool=True):
+        """
+        Pushes metric, that can be accessed by :class:`monitorch.lens.LossMetrics`.
+
+        Parameters
+        ----------
+        name : str
+            Name of the metric to save.
+        value : float
+            Metric's value.
+        running : bool = True
+            Flag indicating if metric should be saved in-place (True) or in-memory (False).
+        """
         if running:
             self._call_preprocessor.push_running(name, value)
         else:
             self._call_preprocessor.push_memory(name, value)
 
     def push_loss(self, value : float, *, train : bool, running : bool = True):
+        """
+        Pushes loss, that can be accessed by :class:`monitorch.lens.LossMetrics`.
+
+        Parameters
+        ----------
+        value : float
+            Loss value.
+        train : bool
+            Flag indicating if it is training loss.
+        running : bool = True
+            Flag indicating if metric should be saved in-place (True) or in-memory (False).
+        """
         self._call_preprocessor.push_loss(value, train=train, running=running)
 
     def tick_epoch(self, epoch : int|None=None):
+        """
+        Ticks epoch to postprocess data and draw plots.
+
+        Parameters
+        ----------
+        epoch : int|None = None
+            Optional epoch counter, default is incremental :attr:`epoch_counter`.
+        """
         if epoch is not None:
             self.epoch_counter = epoch
-        for lens in self._lenses:
+        for lens in self.lenses:
             lens.finalize_epoch()
             lens.vizualize(self.visualizer, self.epoch_counter)
             lens.reset_epoch()
@@ -72,10 +217,30 @@ class PyTorchInspector:
 
     @staticmethod
     def _decide_prefix(prefix : str, grand_name : str):
+        """ Utility function for depth=0 name composition. """
         return prefix if grand_name else ''
 
     @staticmethod
     def _module_leaves(module : Module, depth : int = -1, prefix : str = '.') -> list[tuple[Module, str]]:
+        """
+        A function to extract nodes at defined depth from module inclusion tree.
+        If ``depth=-1`` calls :meth:`_module_deep_leaves`,
+        otherwise recursively goes down the tree decreasing depth.
+
+        Parameters
+        ----------
+        module : torch.nn.Module
+            Module which inclusion tree must be unfolded.
+        depth : int = -1
+            Depth to which the module must be unfolded, default is -1, i.e., until leaf nodes.
+        prefix : str = '.'
+            Delimiter to separate names of parent and child modules.
+
+        Returns
+        -------
+        list[tuple[Module, str]]
+            List of module object and their path name.
+        """
         assert depth >= -1, "Depth of leaves must be non-negative or -1 (maximal depth)"
         if depth == -1:
             return PyTorchInspector._module_deep_leaves(module, prefix=prefix)
@@ -89,6 +254,23 @@ class PyTorchInspector:
 
     @staticmethod
     def _module_deep_leaves(module : Module, prefix : str) -> list[tuple[Module, str]]:
+        """
+        A function to extract leaves from module inclusion tree.
+
+        The function is recursive.
+
+        Parameters
+        ----------
+        module : torch.nn.Module
+            Module which inclusion tree must be unfolded.
+        prefix : str = '.'
+            Delimiter to separate names of parent and child modules.
+
+        Returns
+        -------
+        list[tuple[Module, str]]
+            List of module object and their path name.
+        """
         ret = []
         for name, child in module.named_children():
             ret += [(module, name + PyTorchInspector._decide_prefix(prefix, grand_name) + grand_name) for module, grand_name in PyTorchInspector._module_deep_leaves(child, prefix=prefix)]
